77b3fdaf30a4bc31e2dc2b3bc67838afcac04a42
Fahim-KUET101 <fahimcsekuet@gmail.com>
2023-01-31 21:06:20 +0600

    This is the second commit(for reviewing)

--- blockchain.go
+++ blockchain.go
@@ -1,84 +1,151 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
-	"encoding/json"
+	"encoding/gob"
 	"fmt"
+	"log"
 	"strconv"
-	"strings"
 	"time"
 )
 
+// TransactionData contains the properties of a transaction
+type TransactionData struct {
+	Sender   string
+	Receiver string
+	Amount   int64
+	Time     int64
+}
+
+// Block contains the properties of a block in the blockchain
 type Block struct {
-	data         map[string]interface{}
-	hash         string
-	previousHash string
-	timestamp    time.Time
-	pow          int
-}
-type Blockchain struct {
-	genesisBlock Block
-	chain        []Block
-	difficulty   int
+	Timestamp     int64
+	Transaction   TransactionData
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
 }
 
-func (b Block) calculateHash() string {
-	data, _ := json.Marshal(b.data)
-	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
-	blockHash := sha256.Sum256([]byte(blockData))
-	return fmt.Sprintf("%x", blockHash)
-}
-func (b *Block) mine(difficulty int) {
-	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
-		b.pow++
-		b.hash = b.calculateHash()
-	}
-}
-func CreateBlockchain(difficulty int) Blockchain {
-	genesisBlock := Block{
-		hash:      "0",
-		timestamp: time.Now(),
-	}
-	return Blockchain{
-		genesisBlock,
-		[]Block{genesisBlock},
-		difficulty,
-	}
-}
-func (b *Blockchain) addBlock(from, to string, amount float64) {
-	blockData := map[string]interface{}{
-		"from":   from,
-		"to":     to,
-		"amount": amount,
-	}
-	lastBlock := b.chain[len(b.chain)-1]
-	newBlock := Block{
-		data:         blockData,
-		previousHash: lastBlock.hash,
-		timestamp:    time.Now(),
-	}
-	newBlock.mine(b.difficulty)
-	b.chain = append(b.chain, newBlock)
+// NewBlock creates a new block with the given transaction and previous block hash
+func NewBlock(transaction TransactionData, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), transaction, prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
 }
 
-// // func (b Blockchain) isValid() bool {
-// // 	for i := range b.chain[1:] {
-// // 		previousBlock := b.chain[i]
-// // 		currentBlock := b.chain[i+1]
-// // 		if currentBlock.hash != currentBlock.calculateHash() || currentBlock.previousHash != previousBlock.hash {
-// // 			return false
-// // 		}
-// // 	}
-// // 	return true
-// }
+// NewGenesisBlock creates a new genesis block with a default transaction
+func NewGenesisBlock() *Block {
+	return NewBlock(TransactionData{"", "", 0, time.Now().Unix()}, []byte{})
+}
+
+// Serialize serializes the block into a byte slice
+func (b *Block) Serialize() []byte {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buf.Bytes()
+}
+
+// HashTransaction hashes the transaction data and returns the resulting hash
+func (t *TransactionData) HashTransaction() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(t)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	hash := sha256.Sum256(result.Bytes())
+	return hash[:]
+}
+
+// BlockChain is a chain of blocks
+type BlockChain struct {
+	Blocks []*Block
+}
+
+// AddBlock adds a new block to the blockchain
+func (bc *BlockChain) AddBlock(transaction TransactionData) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(transaction, prevBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+// NewBlockChain creates a new blockchain with a genesis block
+func NewBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{NewGenesisBlock()}}
+}
+
+// ShowBlock shows the properties of a block
+func ShowBlock(block *Block) {
+	fmt.Printf("Timestamp: %d\n", block.Timestamp)
+	fmt.Printf("Transaction: %s sent %d to %s\n", block.Transaction.Sender, block.Transaction.Amount, block.Transaction.Receiver)
+	fmt.Printf("Prev. Block Hash: %x\n", block.PrevBlockHash)
+	fmt.Printf("Hash: %x\n", block.Hash)
+	fmt.Printf("Nonce: %d\n", block.Nonce)
+}
+
+// ShowBlockchain shows the properties of the entire blockchain
+func ShowBlockchain(bc *BlockChain) {
+	for _, block := range bc.Blocks {
+		fmt.Println("---Block---")
+		ShowBlock(block)
+	}
+}
+
+// ProofOfWork represents the proof-of-work algorithm
+type ProofOfWork struct {
+	Block  *Block
+	Target []byte
+}
+
+// NewProofOfWork creates a new proof-of-work object for the given block
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := make([]byte, 32)
+	for i := 0; i < 32; i++ {
+		target[i] = 0xff
+	}
+	return &ProofOfWork{b, target}
+}
+
+// Run performs the proof-of-work algorithm
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var nonce int
+	var hash [32]byte
+	block := pow.Block
+
+	for {
+		data := append(block.Serialize(), intToHex(nonce)...)
+		hash = sha256.Sum256(data)
+
+		if bytes.Compare(hash[:], pow.Target) == -1 {
+			break
+		}
+		nonce++
+	}
+
+	return nonce, hash[:]
+}
+
+func intToHex(num int) []byte {
+	return []byte(strconv.FormatInt(int64(num), 16))
+}
+
 func main() {
-	// create a new blockchain instance with a mining difficulty of 2
-	blockchain := CreateBlockchain(2)
+	bc := NewBlockChain()
 
-	// record transactions on the blockchain for Alice, Bob, and John
-	blockchain.addBlock("fahim", "ahmed", 5)
-	blockchain.addBlock("raihan", "sayeed", 2)
+	bc.AddBlock(TransactionData{"A", "B", 100, time.Now().Unix()})
+	bc.AddBlock(TransactionData{"B", "C", 200, time.Now().Unix()})
 
-	// check if the blockchain is valid; expecting true
-	fmt.Println(blockchain)
+	fmt.Println("---Blockchain---")
+	ShowBlockchain(bc)
 }
